1a60d4174d94d9b0b874bd38bbf953f7ef040c68
Andriamihaja <manu.andriamihaja-razafimanjato@etu.umontpellier.fr>
2025-03-29 11:31:18 +0100

    Liste de tâches fonctionnel

--- js/script.js
+++ js/script.js
@@ -1,25 +1,69 @@
 const boutonPlus = document.getElementById("plus");
 const listeTaches = document.getElementById("liste-tache")
+const creationTache = document.getElementById("creation-tache")
 
+let cpt = 1;
+cacher(creationTache)
 
 boutonPlus.addEventListener("click", function () {
-    listeTaches.innerHTML = `Nom de la tâche <input type="text" name="nomTache" id="nomTache" > 
-                       Description de la tâche <input type="text" name="descTache" id="descTache">
-                       Tâche importante <input type="checkbox" name="tacheImportante" id="important"> 
-                        <input type="button" value="Valider" id="valider">`;
+    if (cpt % 2 !== 0) {
+        cpt++;
+        afficher(creationTache)
+        creationTache.innerHTML =
+            `<div class="div-nomTache">Nom de la tâche
+            <br>
+            <input type="text" name="nomTache" id="nomTache" ></div>
+            <div class="descriptionTache"> Description de la tâche 
+            <br>
+            <input type="text" name="descTache" id="descTache"></div>
+            <div>Tâche importante
+            <br> 
+            <input type="checkbox" name="tacheImportante" id="important"></div>
+             <div class="boutonValider"> <input type="button" value="Valider" id="valider"></div>`;
 
-    const boutonValider = document.getElementById("valider");
-    boutonValider.addEventListener("click", function () {
+        const boutonValider = document.getElementById("valider");
+        boutonValider.addEventListener("click", function () {
 
-        let nomTache = document.getElementById("nomTache");
-        let description = document.getElementById("descTache");
-        let importante = document.getElementById("important");
+            let nomTache = document.getElementById("nomTache");
+            let description = document.getElementById("descTache");
+            let importante = document.getElementById("important");
 
-        listeTaches.innerHTML = `${nomTache.value}<br>
-                                ${description.value}`;
+            listeTaches.innerHTML +=
+                `<div id="div-tache"><strong class="nomDeLaTache">${nomTache.value}</strong>
+                 <br>
+                 ${description.value}
+                 <br>
+                 ${importante.checked ? 'Tâche importante !' : ''} 
+                 <button class="moins"> X </button> </div>`;
 
-        if (importante.checked) {
-            listeTaches.innerHTML += `<br> Tâche importante !`;
-        }
-    });
+            let boutonsMoins = document.querySelectorAll(".moins");
+
+            boutonsMoins.forEach(function(bouton) {
+                console.log("fefefefefefef");
+                bouton.addEventListener("click", function () {
+                    cacher(baliseActuel)
+                });
+            });
+
+
+            listeTaches.innerHTML += `<br>`;
+
+        });
+    } else {
+        cpt++;
+        cacher(creationTache)
+    }
 });
+
+
+function cacher(element) {
+    element.classList.add('cache');
+}
+
+function afficher(element) {
+    element.classList.remove('cache')
+}
+
+function supprimer(element) {
+    element.remove()
+}
